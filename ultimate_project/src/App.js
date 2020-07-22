da1ade2174aa6753a2092c641b0bad593adb81d8
Cyber Slug <lasminski@mail.ru>
2020-07-22 23:32:24 +0300

    ee

--- ultimate_project/src/App.js
+++ ultimate_project/src/App.js
@@ -1,39 +1,21 @@
 import React from 'react';
 import './App.css';
-import { Post } from './components/post';
 
+
+
+import { Header } from './components/header';
+
+
+import { MainContent } from './components/Main-content'
 function App() {
   return (
     <div className="App">
-      <header className={'header'}>header</header>
-      <div className={'main-content'}>
-        <div className={'left-sidebar'}>
-          your walls
-          your something
-          your something
-          your something
-        </div>
-        <div className={'walls'}>
-          <div className={'posts'}>
-            <div className={'post'}>
-              post commit
-          </div>
-            <div className={'post'}>
-              post
-          </div>
-            <Post postTitle={'post title'}/>
-          </div>
-          <div className={'right-sidebar'}>
-            something else
-            something else
-            something else
-            something else
-        </div>
-        </div>
-      </div>
+      <Header header={'хедер'} />
+      <MainContent/>
+      
+     
     </div>
   );
 }
 
 export default App;
-// халтура 
