81100ece3bc42f6e9f6f110cb622bb4467c488d5
AlekseiBY <aleksei.kupchinskii@gmail.com>
2020-07-26 13:26:35 +0300

    nothing

--- ultimate_project/src/components/Main-content/main-content.jsx
+++ ultimate_project/src/components/Main-content/main-content.jsx
@@ -12,7 +12,7 @@ export const MainContent = ({state, dispatch}) => {
     return (
         <div className={style.mainContent}>
                 {/* <Route path={'/myWall'} component={()=><MyWall state={state} dispatch={dispatch}/>} /> */}
-                <Route path={'/walls/walls'} component={()=><Walls state={state} dispatch={dispatch}/>} />
+                <Route path={'/walls'} component={()=><Walls state={state} dispatch={dispatch}/>} />
                 <Route path={'/myTreds'} component={()=><Treds/>} />
                 <Route path={'/profile'} component={()=><Profile state={state.profile} dispatch={dispatch}/>} />
         </div>
